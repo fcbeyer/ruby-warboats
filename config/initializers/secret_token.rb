b274c89fd26f8fbd00a171cea6517ba7c3ade9ea
Rick Beyer <moistd@gmail.com>
2012-01-14 13:57:48 -0500

    initial commit with user

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyWarboats::Application.config.secret_token = '6eac5cca478d912f86421dfad97876ca8a54a819c1d3183121ff7444881a79b122e02c101f62be6eaac8d08631e6b0b11aabef72c06fd17a509c5eed83db83c3'
