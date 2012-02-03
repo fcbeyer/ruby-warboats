c30ad0c3ca95b277c40f5a491fee6b22be8a91ce
Rick Beyer <moistd@gmail.com>
2012-02-03 07:41:40 -0500

    corrected ship validation, added vertical prompt to new ship form

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -3,19 +3,19 @@ class Ship < ActiveRecord::Base
   belongs_to :player
   belongs_to :game
   
+  validates :x, :numericality => {:greater_than_or_equal_to => 0}
+  validates :x, :numericality => {:less_than => 11}
+  validates :y, :numericality => {:greater_than_or_equal_to => 0}
+  validates :y, :numericality => {:less_than => 11}  
   validate :valid_ship_location, :on => :create
   
   def valid_ship_location
-    if self.x < 0 || self.x > 10
-      errors.add(:x, "x coordinate is invalid, must be within 10x10 grid")
-    elsif self.y < 0 || self.y > 10
-      errors.add(:y, "y coordinate is invalid, must be within 10x10 grid")
-    elsif self.vertical?
-      if self.y+self.health > 10
+    if self.vertical?
+      if (self.y + self.health) > 10
         errors.add(:y, " coordinate to high with this ship\'s length")
       end
-    elsif !self.vertical?
-      if self.x+self.health > 10
+    else
+      if (self.x + self.health) > 10
         errors.add(:x, "x coordinate to high with this ship\'s length")
       end
     end
